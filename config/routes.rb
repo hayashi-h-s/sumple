fda873ae286f45679c2e2b1af731cdee2fed92a2
hayamizhin <max9173627>
2020-03-11 14:53:30 +0900

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home/top'
+  root 'home#top'
 end
